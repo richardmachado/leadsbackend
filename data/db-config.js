fbda09a66cf7890524cf8d3573bc56b2e82621d5
Richard Machado <mmachado.richard@gmail.com>
2020-08-16 15:21:27 -0700

    ok, let see if this works

--- data/db-config.js
+++ data/db-config.js
@@ -3,4 +3,4 @@ const env = process.env.DB_ENV || 'production'
 
 const knexConfig = require('../knexfile.js');
 
-module.exports = knex(knexConfig.development);
+module.exports = knex(knexConfig[env]);
